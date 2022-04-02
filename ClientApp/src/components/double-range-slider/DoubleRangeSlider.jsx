71c5e8bf3bf968673b560118017cfdb4c3176971
ktp33 <ktp332@yandex.ru>
2022-04-02 14:43:13 +0300

    Search form for home page with double range slider

--- ClientApp/src/components/double-range-slider/DoubleRangeSlider.jsx
+++ ClientApp/src/components/double-range-slider/DoubleRangeSlider.jsx
@@ -2,13 +2,13 @@ import Nouislider from "nouislider-react";
 import "nouislider/distribute/nouislider.css";
 import "./DoubleRangeSlider.scss"
 
-const DoubleRangeSlider = ({minValue, maxValue, onValueChange}) => {
+const DoubleRangeSlider = ({values, onValueChange}) => {
 
     return (
         <div className="double-range__container">
             <Nouislider
-                range={{min: minValue, max: maxValue}}
-                start={[minValue, maxValue]}
+                range={{min: values.minValue, max: values.maxValue}}
+                start={[values.minValue, values.maxValue]}
                 onSlide={e => onValueChange(e)}
                 step={1}
             />
