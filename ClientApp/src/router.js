710417832b6bde8ef70d2c2b8ec8143d16b1645e
MustafaBostanci <87652304+MustafaBostanci@users.noreply.github.com>
2022-04-04 13:34:39 +0300

    New Features,Registration/Login panel

--- ClientApp/src/router.js
+++ ClientApp/src/router.js
@@ -1,12 +1,18 @@
 import {Switch, Redirect, Route} from "react-router-dom";
 
 import Home from "./components/home/Home";
+import Login from "./components/login/Login";
+import Test from "./components/login/sComp";
+
 
 const Routes = () => {
     return (
         <Switch>
             <Route path="/home">
                 <Home />
+            </Route>
+            <Route path="/login">
+                <Test />
             </Route>
             <Route path="/agents">
 
