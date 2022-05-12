c5239d07c8b5cf255cfec3bb961e387257298aca
Egor <ktp332@yandex.ru>
2022-05-12 13:33:53 +0300

    CreateOffer

--- ClientApp/src/components/create-offer/CreateOffer.jsx
+++ ClientApp/src/components/create-offer/CreateOffer.jsx
@@ -8,7 +8,12 @@ const CreateOffer = () => {
             <Header />
             <div className="container">
                 <div className="page__row">
-
+                    <form>
+                        Title <input  /> <br/>
+                        Description <input  /> <br/>
+                        Address <input  /> <br/>
+                        Price per month <input  /> <br/>
+                    </form>
                 </div>
             </div>
         </>
